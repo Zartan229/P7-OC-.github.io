a8389c64b3ec203bb75f7c601b5410e25e8b9858
Maxime Ortuno <maxime.ortuno@hotmail.fr>
2023-01-24 17:05:03 +0100

    page accueil terminer

--- src/components/Header/_Header-m.jsx
+++ src/components/Header/_Header-m.jsx
@@ -2,7 +2,7 @@ import '../../styles/_Header-m.css'
 import Logo from '../../images/LOGO.png'
 export default function Banner() {
     return <header>
-        <img src={Logo} alt="Logo" />
+        <img className='header-logo' src={Logo} alt="Logo" />
         <nav>
             <h2 className="nav_text">Accueil</h2>
             <h2 className="nav_text">A propos</h2>
