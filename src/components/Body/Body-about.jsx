f3ccb895344737ce5d58c923386f369e4954464b
Maxime Ortuno <maxime.ortuno@hotmail.fr>
2023-01-30 15:40:42 +0100

    A propos

--- src/components/Body/Body-about.jsx
+++ src/components/Body/Body-about.jsx
@@ -1,5 +1,5 @@
 
-import '../../styles/_Body-about.css'
+import './style.body-collapse.css'
 import Collapse from '../Collapse/Collapse'
 
 
@@ -8,7 +8,10 @@ export default function BodyAbout() {
 
     return<> 
     <section className='section2-about'>
-    <Collapse title="Fiabilité" description="Hello world!"/>
+    <Collapse title="Fiabilité" description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."/>
+    <Collapse title="Fiabilité" description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
+    <Collapse title="Fiabilité" description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."/>
+    <Collapse title="Fiabilité" description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."/>
     </section>
     </>
   }
